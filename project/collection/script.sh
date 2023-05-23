7d7aac85dbdde132e6268ff53c86659bfa31c3dc
marchgroup2023 <march23devops@gmail.com>
2023-05-23 01:23:54 +0000

    Done

--- project/collection/script.sh
+++ project/collection/script.sh
@@ -7,24 +7,24 @@ API_KEY="c691db3bf95da15d1cdb226cc32e229bd5618569"
 # Set the collection name, version, and tarball filename
 COLLECTION_NAME="project"
 COLLECTION_VERSION="1.0.0"
-TARBALL_FILENAME="$project-collection-1.0.0.tar.gz"
+TARBALL_FILENAME="project-collection-${COLLECTION_VERSION}.tar.gz"
 
 # Build the collection tarball
-ansible-galaxy collection build $project-collection-1.0.0.tar.gz --force --output-path=./
+ansible-galaxy collection build --force --output-path=./
 
 # Check if the tarball was created successfully
-if [ -f "$project-collection-1.0.0.tar.gz" ]; then
-  echo "Collection tarball created successfully: $project-collection-1.0.0.tar.gz"
+if [ -f "${TARBALL_FILENAME}" ]; then
+  echo "Collection tarball created successfully: ${TARBALL_FILENAME}"
 else
   echo "Failed to create collection tarball"
   exit 1
 fi
 
 # Login to Ansible Galaxy
-ansible-galaxy login --token "$c691db3bf95da15d1cdb226cc32e229bd5618569"
+ansible-galaxy login --token "${API_KEY}"
 
 # Publish the collection to Ansible Galaxy
-ansible-galaxy collection publish --token "$c691db3bf95da15d1cdb226cc32e229bd5618569" "$project-collection-1.0.0.tar.gz" -r marchgroup
+ansible-galaxy collection publish --token "${API_KEY}" "${TARBALL_FILENAME}" -r marchgroup
 
 # Check if the publishing was successful
 if [ $? -eq 0 ]; then
